d68b7d5218e9ef0afa9ea26c33beb9c188ca2dda
Emily Gordon <egordon@pivotal.io>
2018-07-25 16:26:58 -0700

    add elm debugging to local development
    Signed-off-by: Augustus Lidaka <alidaka@pivotal.io>

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,9 @@ module.exports = {
         test:    /\.elm$/,
         exclude: [/elm-stuff/, /node_modules/],
         loader:  'elm-webpack-loader?verbose=true&warn=true',
+        options: {
+          debug: true,
+        }
       },
     ],
 
